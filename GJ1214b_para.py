a8420dd382911f9c3ebcd013957232213b95a816
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2018-05-30 15:33:57 +0200

    Add files via upload

--- GJ1214b_para.py
+++ GJ1214b_para.py
@@ -111,12 +111,14 @@ else :
 if data_base != '' :
     T_iso, P_surf = 0,0
     x_ratio_species_active = np.array([0,0,0,0,0])
+    M_species, M, x_ratio_species = ratio(n_species,x_ratio_species_active,IsoComp=False)
     #x_ratio_species_active = np.array([0,0])
 else :
     T_iso, P_surf = information[planet.planet_temperature_key], information[planet.extreme_pressure_key[0]]
     x_ratio_species_active = information[planet.planet_active_ratio_key]
+    M_species, M, x_ratio_species = ratio(n_species,x_ratio_species_active,IsoComp=True)
 
-M_species, M, x_ratio_species = ratio(n_species,x_ratio_species_active,IsoComp=True)
+
 
 # Proprietes des nuages
 
