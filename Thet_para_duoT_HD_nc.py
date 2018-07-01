9bd57816f0dba0edcfc6951908e9167ad4e31182
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2018-07-02 01:42:03 +0200

    Add files via upload

--- Thet_para_duoT_HD_nc.py
+++ Thet_para_duoT_HD_nc.py
@@ -31,7 +31,7 @@ number_rank = comm.size
 # Informations diverses sur l'etude
 
 path = "/data1/caldas/Pytmosph3R/"
-name_file = "Files/Para_500_1000_NI_nc"
+name_file = "Files/Para_500_1000_HD_NI_nc"
 name_source = "Source"
 name_exo = "HD209458"
 #name_exo = "GJ1214b"
@@ -295,7 +295,7 @@ Flux = False            ###### Spectre flux = f(longueur d'onde)
 
 # Sauvegardes
 
-save_adress = "/data1/caldas/Pytmosph3R/I_500_1000_NI_nc/"
+save_adress = "/data1/caldas/Pytmosph3R/I_500_1000_HD_NI_nc/"
 special = ''
 if rank == 0 :
     stud = stud_type(r_eff,Single,Continuum,Molecular,Scattering,Clouds)
